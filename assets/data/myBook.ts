bea75fe33f56d69dddfe2d618ef0fd9f8a4b79b8
marfatab <52579201+marfatab@users.noreply.github.com>
2019-07-26 21:00:04 -0400

    Add files via upload

---
+++ assets/data/myBook.ts
@@ -0,0 +1,8 @@
+import {BookInfo} from '../../app/bookInfo';
+export const MY_BK: BookInfo[] =
+[
+    {bk_name: 'Complete works', bk_Author: 'Modern Library', bk_Genre:'Folio',yr_Published: '2015', picture:'bk_1.png'},
+    {bk_name: 'Alls well that ends well', bk_Author: 'William Shakespeare', bk_Genre:'Letter Series',yr_Published: '2006', picture:'bk_2.png'},
+    {bk_name: 'Collins Little book of shakesphere', bk_Author: 'Mannion, John', bk_Genre:'Quotations',yr_Published: '2016', picture:'bk_3.png'},
+    {bk_name: 'shakespheres imagery ', bk_Author: 'Caroline Frances Eleanor Spurgeon', bk_Genre:'Story telling',yr_Published: '1971', picture:'bk_4.png'}
+]
